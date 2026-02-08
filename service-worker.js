c184002b7e389c02e791a33ba9e2d94b677b69af
RMking007 <164857784+RMking007@users.noreply.github.com>
2026-02-09 05:21:24 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'grocer-pro-v1';
+const ASSETS = [
+  './grocery_app.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/3724/3724720.png'
+];
+
+// Install Event: Caches the essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event: Cleans up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serves files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
